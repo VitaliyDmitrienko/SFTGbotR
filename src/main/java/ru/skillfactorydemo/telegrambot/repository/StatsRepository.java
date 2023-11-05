317ffeb417583854bbc0334751420bc34ce877a1
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-11-05 15:43:51 +0300

    18th. Replace JDBCTemplate to NamedParameterJdbcTemplate (due to SF Module 35.3.0 second part screencast by Egor Voroniansky).

--- src/main/java/ru/skillfactorydemo/telegrambot/repository/StatsRepository.java
+++ src/main/java/ru/skillfactorydemo/telegrambot/repository/StatsRepository.java
@@ -1,16 +1,30 @@
 package ru.skillfactorydemo.telegrambot.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
 public class StatsRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     public int getCountOfIncomesThatGreaterThan(BigDecimal amount) {
-        return jdbcTemplate.queryForObject("SELECT count(*) FROM INCOMES WHERE income > ?", Integer.class, amount);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("amount", amount);
+        return namedParameterJdbcTemplate.queryForObject("SELECT count(*) FROM INCOMES WHERE income > :amount", parameters, new StatsRowMapper());
+    }
+
+    public static final class StatsRowMapper implements RowMapper<Integer> {
+        @Override
+        public Integer mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            return resultSet.getInt("COUNT");
+        }
     }
 }
