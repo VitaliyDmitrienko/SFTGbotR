cc3fbec6b164408a13e6d32a5b57e4218d07f222
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-11-11 18:39:46 +0300

    24th. IncomeRepositoryTest: before test for-cycle add check quantity pre-test inserted strings (from test/import.sql). SpendRepositoryTest: generated by example (IncomeRepositoryTest).

--- src/test/java/ru/skillfactorydemo/telegrambot/repository/SpendRepositoryTest.java
+++ src/test/java/ru/skillfactorydemo/telegrambot/repository/SpendRepositoryTest.java
@@ -3,29 +3,29 @@ package ru.skillfactorydemo.telegrambot.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import ru.skillfactorydemo.telegrambot.entity.Income;
+import ru.skillfactorydemo.telegrambot.entity.Spend;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
-class IncomeRepositoryTest {
+class SpendRepositoryTest {
 
     @Autowired
-    private IncomeRepository incomeRepository;
+    private SpendRepository spendRepository;
 
     @Test
     public void testRepo() {
         //before this test cycle execute some insertion from test/import.sql
         //noinspection StatementWithEmptyBody
-        List<Income> found = incomeRepository.findAll();
+        List<Spend> found = spendRepository.findAll();
         int importInsertCount = found.size();
         int maxTestStep = 10;
         if (maxTestStep > importInsertCount) {
-            for (int i = 0; i < (maxTestStep - importInsertCount); i++, incomeRepository.save(new Income())) ;
+            for (int i = 0; i < (maxTestStep - importInsertCount); i++, spendRepository.save(new Spend())) ;
         }
-        found = incomeRepository.findAll();
+        found = spendRepository.findAll();
         assertEquals(10, found.size());
     }
+
 }
